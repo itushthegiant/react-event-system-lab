4ad17c522deee83604cd9378e61d98cbc2fe8314
etamar <etamar.levy@gmail.com>
2021-06-05 17:47:57 -0400

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    onFocusFunction = () => {
+        console.log('Good!')
+    }
+
+    onBlurFunction = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button
+                onFocus={this.onFocusFunction}
+                onBlur={this.onBlurFunction}
+                >
+                </button>
+            </div>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+    handleKeyPress = () => {
+            console.log('Entering password...')
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                 <input
+                  onKeyUp={this.handleKeyPress}
+                  type="password"
+                  />
+            </div>
+        )
+    }
+}
